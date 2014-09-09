7acfa324f039f0496e24b7997eac1a4cb4e99d4a
Slicer <slicer256@gmail.com>
2014-09-09 21:33:01 +0300

    primitive payment

--- basket.php
+++ basket.php
@@ -22,6 +22,7 @@ Email: {$_REQUEST[email]}
 				'status' => 2,
 				'timestamp' => time(),
 			),'') or die(mysql_error());
+			$order_id = $db->last_insert_id('orders');
 			
 			$cbasket->clearBasket();
 			$cbasket->save();
@@ -29,40 +30,66 @@ Email: {$_REQUEST[email]}
 			mailNotification('Оформлен новый заказ',$description);
 			add_log('basket', "Order submitted");					
 			
-			header("location: /cart?done");
+			header("location: /cart?done&order=".$order_id);
 		}
 	}
 	
-	
-	
-	$tpl[content] .= "<table class='simpleTable'>";
-	$total_count = $total_amount = 0;
-	foreach ($cbasket->getItems() as $item) {
-		$row = $db->get_row("select * from items where id='".(int)$item[id]."'");		
-		if ($row[price_promo]) $row[price] = $row[price_promo];
-		$tpl[items_sum] += $row[price];
-		$total_count += $item[count];
-		$total_amount += $item[count] * $row[price];
-		$tpl[content] .= "<tr>
-			<td><a href='".format_url('item',$row)."'><img src='/images/$row[id].jpg?width=100'></a></td>
-			<td>$row[name] ".($item[variant]!='' ? "({$item[variant]})" : '')."</td>
-			<td>$row[price] грн</td>
-			<td>
-				<a href='/ajax/index.php?module=basket&action=change_count&id=$row[id]&value=-1'><i class='fa fa-minus-square-o'></i></a>
-				<b>$item[count] </b>
-				<a href='/ajax/index.php?module=basket&action=change_count&id=$row[id]&value=1'><i class='fa fa-plus-square-o'></i></a></td>
-			<td><a href='/ajax/index.php?module=basket&action=delete&id=$row[id]' class='delete_from_basket' title='Удалить из корзины'><img src='/img/basic/delete.png' /></a></td>
-			</tr>";
+	elseif (isset($_REQUEST[done])) {
+		$row = $db->get_row("select * from orders where id='".(int)$_REQUEST[order]."'");
+		
+		$tpl[content] = "<h3>Спасибо. Ваш заказ на сумму {$row[price]} грн размещен</h3>
+			Для оплаты перейдите по ссылке ниже: <p>";
+		
+		include "classes/liqpay.php";
+		
+		$liqpay = new LiqPay('i97839051443', 'VT9XujjLgS3LW3o6CQTFtIUkgixS8mvoTZMZSHgb');
+		$tpl[content] .= $liqpay->cnb_form(array(
+			'amount'         => $row[price],
+			'currency'       => 'UAH',
+			'description'    => "Оплата заказа №".$row[id]." на сумму {$row[price]} грн",
+			'order_id'       => 'order_id_1',
+			'type'           => 'buy',
+			'result_url'	 => 'http://figli-migli.net/cart?complete',
+			'language'		 => 'ru',
+			'sandbox'		 => '1',
+		));
+		//$tpl[content] .= get_tpl('basket_pay.tpl');
 	}
-	$tpl[items_sum] = $total_amount;
-	$tpl[content] .= "
-		<tr><td colspan='5' style='text-align:center'>Всего товаров: <b>$total_count</b>, общая стоимость: <b>$total_amount</b> грн</td></tr>
-		</table>";
-	$tpl[items] = serialize($cbasket->getItems());
 	
-	$tpl[content] = get_tpl('basket.tpl');
-	
-	if (isset($_REQUEST[done])) {
-		$tpl[sys_message] = "Спасибо. Ваш заказ размещен";
+	elseif (isset($_REQUEST[complete])) {
+		$tpl[sys_message] .= "Ваш заказ оплачен! Менеджер магазина свяжется с вами в ближайшее время.";
 	}
+	
+	else {
+	
+		$tpl[content] .= "<table class='simpleTable'>";
+		$total_count = $total_amount = 0;
+		foreach ($cbasket->getItems() as $item) {
+			$row = $db->get_row("select * from items where id='".(int)$item[id]."'");		
+			if ($row[price_promo]) $row[price] = $row[price_promo];
+			$tpl[items_sum] += $row[price];
+			$total_count += $item[count];
+			$total_amount += $item[count] * $row[price];
+			$tpl[content] .= "<tr>
+				<td><a href='".format_url('item',$row)."'><img src='/images/$row[id].jpg?width=100'></a></td>
+				<td>$row[name] ".($item[variant]!='' ? "({$item[variant]})" : '')."</td>
+				<td>$row[price] грн</td>
+				<td>
+					<a href='/ajax/index.php?module=basket&action=change_count&id=$row[id]&value=-1'><i class='fa fa-minus-square-o'></i></a>
+					<b>$item[count] </b>
+					<a href='/ajax/index.php?module=basket&action=change_count&id=$row[id]&value=1'><i class='fa fa-plus-square-o'></i></a></td>
+				<td><a href='/ajax/index.php?module=basket&action=delete&id=$row[id]' class='delete_from_basket' title='Удалить из корзины'><img src='/img/basic/delete.png' /></a></td>
+				</tr>";
+		}
+		$tpl[items_sum] = $total_amount;
+		$tpl[content] .= "
+			<tr><td colspan='5' style='text-align:center'>Всего товаров: <b>$total_count</b>, общая стоимость: <b>$total_amount</b> грн</td></tr>
+			</table>";
+		$tpl[items] = serialize($cbasket->getItems());
+		
+		$tpl[content] = get_tpl('basket.tpl');
+	
+	}
+	
+	
 ?>
---
+++ tpl/basket_pay.tpl
@@ -0,0 +1,15 @@
+
+<form method="POST" accept-charset="utf-8" action="https://www.liqpay.com/api/pay">
+<input type="hidden" name="public_key" value="i97839051443" />
+<input type="hidden" name="amount" value="5" />
+<input type="hidden" name="currency" value="UAH" />
+<input type="hidden" name="description" value="efefef" />
+<input type="hidden" name="type" value="buy" />
+<input type="hidden" name="pay_way" value="card,delayed" />
+<input type="hidden" name="server_url" value="http://figli-migli.net/cart?complete" />
+<input type="hidden" name="order_id" value="123123" />
+<input type="hidden" name="language" value="ru" />
+<input type="hidden" name="sandbox" value="1" />
+<input type="hidden" name="signature" value="1" />
+<input type="image" src="//static.liqpay.com/buttons/p1ru.radius.png" name="btn_text" />
+</form>
